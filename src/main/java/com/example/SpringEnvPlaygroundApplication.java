98de88d4cfafff7a7c81f8e9c510855494a057c5
kiyota <kiyota@tagbangers.co.jp>
2022-07-06 11:24:14 +0900

    init commit

---
+++ src/main/java/com/example/SpringEnvPlaygroundApplication.java
@@ -0,0 +1,31 @@
+package com.example;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+
+@SpringBootApplication
+@EnableConfigurationProperties(SampleProperties.class)
+public class SpringEnvPlaygroundApplication implements CommandLineRunner {
+
+    private final SampleProperties sampleProperties;
+
+    public SpringEnvPlaygroundApplication(SampleProperties sampleProperties) {
+        this.sampleProperties = sampleProperties;
+    }
+
+    public static void main(String[] args) {
+        SpringApplication app = new SpringApplication(SpringEnvPlaygroundApplication.class);
+        app.run();
+    }
+
+    public void run(String... args) throws Exception {
+        System.out.println("hello");
+        System.out.println("using environment: " + sampleProperties.getEnvironment());
+        System.out.println("name: " + sampleProperties.getName());
+        System.out.println("enabled:" + sampleProperties.isEnabled());
+        System.out.println("servers: " + sampleProperties.getServers());
+    }
+}
