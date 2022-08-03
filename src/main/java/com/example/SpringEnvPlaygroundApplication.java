bf5a049a4b08407a7a761b8c17ca51c25ccb7735
kiyota <kiyota@tagbangers.co.jp>
2022-08-03 12:51:08 +0900

    add environment

--- src/main/java/com/example/SpringEnvPlaygroundApplication.java
+++ src/main/java/com/example/SpringEnvPlaygroundApplication.java
@@ -1,10 +1,10 @@
 package com.example;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.core.env.Environment;
 
 @SpringBootApplication
 @EnableConfigurationProperties(SampleProperties.class)
@@ -12,8 +12,11 @@ public class SpringEnvPlaygroundApplication implements CommandLineRunner {
 
     private final SampleProperties sampleProperties;
 
-    public SpringEnvPlaygroundApplication(SampleProperties sampleProperties) {
+    private final Environment environment;
+
+    public SpringEnvPlaygroundApplication(SampleProperties sampleProperties, Environment environment) {
         this.sampleProperties = sampleProperties;
+        this.environment = environment;
     }
 
     public static void main(String[] args) {
@@ -28,5 +31,6 @@ public class SpringEnvPlaygroundApplication implements CommandLineRunner {
         System.out.println("sftp-name: " + sampleProperties.getSftpName());
         System.out.println("enabled:" + sampleProperties.isEnabled());
         System.out.println("servers: " + sampleProperties.getServers());
+        System.out.println("from Environment Variable: " + environment.getProperty("sample.username"));
     }
 }
